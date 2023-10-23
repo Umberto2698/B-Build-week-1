f54ab40755ab764466b3db2e9d86570cca66d865
flaviodeangelis56 <deangelisflavio@gmail.com>
2023-10-23 16:39:41 +0200

    add-relazioni

--- src/main/java/enteties/Mezzi.java
+++ src/main/java/enteties/Mezzi.java
@@ -3,24 +3,22 @@ package enteties;
 import enums.StatoMezzo;
 import enums.TipoMezzo;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Random;
 
 @Entity
 @Table(name = "transport")
 public class Mezzi {
+    @OneToMany(mappedBy = "mezzo")
+    Biglietti biglietto;
+    @OneToMany(mappedBy = "mezzoPeriodo")
+    Periodi periodo;
     @Id
     private long id = new Random().nextLong(1000000000000L, 10000000000000L);
-
     @Column(name = "transport_type")
     private TipoMezzo tipoMezzo;
-
     @Column(name = "transport_state")
     private StatoMezzo statoMezzo = StatoMezzo.IN_SERVIZIO;
-
     @Column(name = "total_seats")
     private int postiTotali;
 
